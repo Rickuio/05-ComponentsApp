d20eb58220d06247c3a6d929e00de64f42af96bf
Ricardo Anchaluisa <ricardoanchaluisa@gmail.com>
2025-01-07 22:16:43 -0500

    Añadir ionicons

---
+++ src/presentation/icons/Icons.tsx
@@ -0,0 +1,4 @@
+import Icon from 'react-native-vector-icons/Ionicons'
+
+export const AirplaneIcon = () => <Icon name="airplane-outline" size={30} />;
+export const BookIcon = () => <Icon name="book-outline" size={30} />;
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,12 @@
 import { Text, View } from 'react-native'
+import { AirplaneIcon, BookIcon } from '../../icons/Icons'
 
 export const HomeScreen = () => {
   return (
     <View>
         <Text>HomeScreen</Text>
+        <AirplaneIcon />
+        <BookIcon />
     </View>
   )
 }
