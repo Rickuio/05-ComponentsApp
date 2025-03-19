49890a56278df09dc37c61b43c57ce29ec78657b
Ricardo Anchaluisa <ricardoanchaluisa@gmail.com>
2025-03-18 20:43:17 -0500

    Animation101Screen

--- src/presentation/screens/animations/Animation101Screen.tsx
+++ src/presentation/screens/animations/Animation101Screen.tsx
@@ -1,73 +1,73 @@
-import React, { useRef } from 'react'
-import { Animated, Easing, Pressable, StyleSheet, Text, View } from 'react-native'
-import { colors } from '../../../config/theme/theme'
+import React, { useRef } from 'react';
+import { Animated, Easing, Pressable, StyleSheet, Text, View } from 'react-native';
+import { colors } from '../../../config/theme/theme';
 
 export const Animation101Screen = () => {
 
     const animatedOpacity = useRef(new Animated.Value(0)).current;
     const animatedTop = useRef(new Animated.Value(-100)).current;
-    
+
     const fadeIn = () => {
 
         Animated.timing( animatedTop, {
             toValue: 0,
             duration: 700,
             useNativeDriver: true,
-            easing: Easing.bounce //Easing.elastic(1)
+            easing: Easing.bounce, //Easing.elastic(1)
         }).start( () => console.log('fadeIn finished') );
 
         Animated.timing( animatedOpacity, {
             toValue: 1,
             duration: 300,
-            useNativeDriver: true
+            useNativeDriver: true,
         }).start( () => console.log('fadeIn finished') );
-    }
+    };
 
     const fadeOut = () => {
 
         Animated.timing( animatedOpacity, {
             toValue: 0,
             duration: 200,
-            useNativeDriver: true
+            useNativeDriver: true,
         }).start( () => {
-            animatedTop.resetAnimation()
-            console.log('fadeOut finished')
+            animatedTop.resetAnimation();
+            console.log('fadeOut finished');
         } );
 
-    }
+    };
 
   return (
     <View style={styles.container}>
-        
+
         <Animated.View style={
-            [styles.purpleBox, { 
+            [styles.purpleBox, {
                 opacity: animatedOpacity,
                 transform: [{
-                    translateY: animatedTop
-                }]
+                    translateY: animatedTop,
+                }],
             }]
         } />
-        
-        <Pressable onPress={ () => fadeIn() } style={{ marginTop: 10 }}> 
+
+        <Pressable onPress={ () => fadeIn() } style={{ marginTop: 10 }}>
             <Text>FadeIn</Text>
         </Pressable>
-        
-        <Pressable onPress={ fadeOut } style={{ marginTop: 10 }}> 
+
+        <Pressable onPress={ fadeOut } style={{ marginTop: 10 }}>
             <Text>FadeOut</Text>
         </Pressable>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     purpleBox: {
         backgroundColor: colors.primary,
         width: 150,
-        height: 150
-    }
-})
+        height: 150,
+    },
+});
